1e758ebdabfe8aeff4b6bbd53392e68cde883266
Your Name <sadaqunps@gmail.com>
2024-05-31 16:56:26 +0530

    learnig operators

---
+++ EqulityVSSrictEq.js
@@ -0,0 +1,21 @@
+
+let esal=40000
+
+let eage =20
+
+console.log(esal>40000 && eage > 25)
+
+
+/*if(esal>45000 && eage>21){
+
+    console.log("Eligible for marrige")
+
+    }
+
+    else{
+        console.log("Not Eligible for marrige")
+    }*/
+
+    
+
+
---
+++ logical.js
@@ -0,0 +1,13 @@
+
+let a=10
+let b='10'
+let c=20
+
+console.log(a==b)  //true
+console.log(a==c)   //false
+
+
+console.log("Strict eqality example")
+
+console.log(a===b) //false
+console.log(a===c)   //false
